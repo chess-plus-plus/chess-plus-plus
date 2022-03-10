b9f01841a913eeee0119bd805e48e96cf6d62a90
Felx Theodor <felix.theodor@rub.de>
2022-03-10 17:00:38 +0100

    first libgdx commit

---
+++ desktop/src/com/chessplusplus/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chessplusplus.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chessplusplus.ChessPlusPlus;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChessPlusPlus(), config);
+	}
+}
