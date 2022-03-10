f2f9069c042ebb41b9f8acb5241effe6e466223e
Felx Theodor <felix.theodor@rub.de>
2022-03-10 17:00:38 +0100

    first libgdx commit

---
+++ android/src/com/chessplusplus/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.chessplusplus;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.chessplusplus.ChessPlusPlus;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ChessPlusPlus(), config);
+	}
+}
