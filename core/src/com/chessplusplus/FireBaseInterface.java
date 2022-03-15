4e054012a72c6c118eb63d86fa93022e880a59e3
Felx Theodor <felix.theodor@rub.de>
2022-03-15 11:07:57 +0100

    merge branch Firebase

---
+++ core/src/com/chessplusplus/FireBaseInterface.java
@@ -0,0 +1,8 @@
+package com.chessplusplus;
+
+public interface FireBaseInterface {
+    void Status();
+    void FirstFireBaseTest();
+    void SetOnValueChangedListener();
+    void SetValueInDb(String target, String value);
+}
