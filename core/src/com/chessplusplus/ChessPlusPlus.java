b6cbefeff3d102f55347f9f0ee4e1aa1972fe250
Felx Theodor <felix.theodor@rub.de>
2022-03-23 13:02:55 +0100

    comment out a line to prevent crash

--- core/src/com/chessplusplus/ChessPlusPlus.java
+++ core/src/com/chessplusplus/ChessPlusPlus.java
@@ -27,7 +27,7 @@ public class ChessPlusPlus extends ApplicationAdapter implements ApplicationList
 		String gameID = "example-game-123";
 		_FBIC.sendInitialState(gameID, "A3B4");
 		_FBIC.getGameUpdates(gameID);
-		game = new Game();
+		//game = new Game();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		_FBIC.sendMove(gameID, "1 C5");
