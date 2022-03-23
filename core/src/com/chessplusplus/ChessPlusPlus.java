f21bf69e841b5e2320f96f097b89ba96512cbb5c
jonathni <jonathni@stud.ntnu.no>
2022-03-23 13:03:58 +0100

    Start app with menu from ChessPLusPlus

--- core/src/com/chessplusplus/ChessPlusPlus.java
+++ core/src/com/chessplusplus/ChessPlusPlus.java
@@ -20,6 +20,8 @@ public class ChessPlusPlus extends ApplicationAdapter implements ApplicationList
 	Game game;
 	BoardView boardView;
 
+	ApplicationAdapter screen;
+
 	public ChessPlusPlus(FireBaseInterface FBIC) {_FBIC = FBIC;}
 	
 	@Override
@@ -28,28 +30,34 @@ public class ChessPlusPlus extends ApplicationAdapter implements ApplicationList
 		_FBIC.sendInitialState(gameID, "A3B4");
 		_FBIC.getGameUpdates(gameID);
 		game = new Game();
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//batch = new SpriteBatch();
+		//img = new Texture("badlogic.jpg");
 		_FBIC.sendMove(gameID, "1 C5");
 		_FBIC.sendMove(gameID, "2 D6");
 
 		Gdx.input.setInputProcessor(this);
 
-		boardView = new BoardView(batch);
+		//boardView = new BoardView(batch);
+
+		screen = new StartMenu();
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0.5f, 0.5f, 0.5f, 1);
-		batch.begin();
-		boardView.render(Gdx.graphics.getDeltaTime());
-		batch.end();
+
+		screen.render();
+
+		//ScreenUtils.clear(0.5f, 0.5f, 0.5f, 1);
+		//batch.begin();
+		//boardView.render(Gdx.graphics.getDeltaTime());
+		//batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		//batch.dispose();
+		//img.dispose();
 	}
 
 	@Override
