182070d6f79e4514d2e4b771522c469764f46419
Ferejbo <felix.rej.bottolfsen@gmail.com>
2022-04-20 13:43:44 +0200

    Font helping

--- core/src/com/chessplusplus/game/utils/FontUtils.java
+++ core/src/com/chessplusplus/game/utils/FontUtils.java
@@ -12,8 +12,23 @@ public class FontUtils {
      * @return height in pixels of text in BitmapFont
      * */
     public static float getHeightOfFontText(String text, BitmapFont bitmapFont) {
+        return getGlyph(text, bitmapFont).height;
+    }
+
+    /**
+     * Calculates width (in pixels) of text in a chosen BitmapFont
+     * @param text Text to evaluate height of
+     * @param bitmapFont BitmapFont to consider in height calculation
+     * @return height in pixels of text in BitmapFont
+     * */
+    public static float getWidthOfFontText(String text, BitmapFont bitmapFont) {
+        return getGlyph(text, bitmapFont).width;
+    }
+
+    private static GlyphLayout getGlyph(String text, BitmapFont bitmapFont) {
         GlyphLayout layout = new GlyphLayout();
         layout.setText(bitmapFont, text);
-        return layout.height;
+        return layout;
     }
+
 }
